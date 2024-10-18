1272103e5d91188b71c390e2c9c65dbd5656f014
Hacker <my@gmail.com>
2024-10-18 14:50:43 -0400

    4 lab

--- policlinic/app/urls.py
+++ policlinic/app/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('admin/', admin.site.urls),
+ 
     path("accounts/", include("django.contrib.auth.urls")),
 
     # Добавляем имя маршруту
--- policlinic/policlinic/settings.py
+++ policlinic/policlinic/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qb0)ev^8@@mt(bu!z6(fi1h(&5m(f6pjfeo!8zgc&qlk!%yr5g'
+SECRET_KEY = os.environ.get('SECRET_KEY_APP', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'policlinic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', ''),  # Измените на переменную окружения
+        'USER': os.environ.get('MYSQL_USER', ''),  # Измените на переменную окружения
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),  # Измените на переменную окружения
+        'HOST': 'db',  # Используйте имя сервиса
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
